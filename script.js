477724ead2c80cc5c1fa562a184669dcece99422
Kiri <kirilynne@gmail.com>
2021-10-02 14:10:36 -0400

    add pseudocode to script.js with required features

---
+++ script.js
@@ -0,0 +1,7 @@
+//create a form with an "input"
+//put in city--on click "Search", current weather and next 5 day forecast is displayed
+    //current weather: city name, date, an icon to represent the weather conditions, temperature, humidity, wind speed, uv index
+        //uv index is color coded based on favorable, moderate or severe --- look up colors!
+    //future weather: dates, icon representing each day's conditions, temperature, humidity
+//that city is added to search history (local storage)
+//clicking the city in the search history brings up the conditions
