53a078a26bbedf1842c2a6d13acb135074906e8a
Kiri <kirilynne@gmail.com>
2021-10-02 14:12:50 -0400

    pseudocode functions needed for script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@
     //future weather: dates, icon representing each day's conditions, temperature, humidity
 //that city is added to search history (local storage)
 //clicking the city in the search history brings up the conditions
+
+//click event -- to input city
+
+//fetch geo for lat and long to use
+//fetch that city's data by linking to geo results
+//display required elements
+
+//store that city in local storage
